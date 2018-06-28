f5e756e7343a608d5d4ccce5890fc3cc7d99fd5c
DezkaReid <elmaildeldezkareid@gmail.com>
2018-06-27 20:56:38 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const appContainer = document.getElementById('app-container')
+
+ReactDOM.render(<h1>Memeflix</h1>, appContainer)
---
+++ webpack.common.js
@@ -0,0 +1,29 @@
+const path = require('path')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
+
+const config = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['react', 'env']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: path.resolve(__dirname, 'src/index.html'),
+      filename: './index.html'
+    }),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ]
+}
+
+module.exports = config
